f24e22608837e8c4b271e43e2f9b8876ac676c76
KAHILER0528 <jveliz0528@gmail.com>
2025-03-19 17:43:20 -0300

    se crea el diseño del mensaje - clase 56

--- src/components/chat/ChatMessages.vue
+++ src/components/chat/ChatMessages.vue
@@ -2,17 +2,16 @@
   <div class="flex-1 overflow-y-auto p-4">
     <div class="flex flex-col space-y-2">
       <!-- Messages go here -->
-      <!-- Example Message -->
-      <div class="flex justify-end">
-        <div class="bg-blue-200 text-black p-2 rounded-lg max-w-xs">Hey, how's your day going?</div>
-      </div>
-
-      <!-- Example Received Message -->
-      <div class="flex">
-        <div class="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
-          Not too bad, just a bit busy. How about you?
-        </div>
-      </div>
+      <ChatBubble :its-mine="true" message="Hello Word" />
+      <ChatBubble
+        :its-mine="false"
+        :message="'no'"
+        image="https://yesno.wtf/assets/yes/14-b57c6dc03aa15a4b18f53eb50d6197ee.gif"
+      />
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import ChatBubble from '@/components/chat/ChatBubble.vue';
+</script>

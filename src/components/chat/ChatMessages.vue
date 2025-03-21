67a2cbbb2b9d3ede39b02f98696fec33961c4c9f
KAHILER0528 <jveliz0528@gmail.com>
2025-03-21 18:59:26 -0300

    Se hace referencian al elemento html y se crea el scroll - clase 61

--- src/components/chat/ChatMessages.vue
+++ src/components/chat/ChatMessages.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-1 overflow-y-auto p-4">
+  <div ref="chatRef" class="flex-1 overflow-y-auto p-4">
     <div class="flex flex-col space-y-2">
       <!-- Messages go here -->
       <ChatBubble v-for="msg in messages" :key="msg.id" v-bind="msg" />
@@ -16,10 +16,22 @@
 <script setup lang="ts">
 import ChatBubble from '@/components/chat/ChatBubble.vue';
 import type { ChatMessage } from '@/interfaces/Chat-message.interface';
+import { ref, watch } from 'vue';
 
 interface Props {
   messages: ChatMessage[];
 }
 
-defineProps<Props>();
+const props = defineProps<Props>();
+
+const chatRef = ref<HTMLDivElement | null>(null);
+
+watch(props.messages, () => {
+  setTimeout(() => {
+    chatRef.value?.scrollTo({
+      top: chatRef.value.scrollHeight,
+      behavior: 'smooth',
+    });
+  }, 100);
+});
 </script>

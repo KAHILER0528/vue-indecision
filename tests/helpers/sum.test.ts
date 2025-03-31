f59124bfbc793dd4ccf11c4290c871fcee12959a
KAHILER0528 <jveliz0528@gmail.com>
2025-03-31 15:41:27 -0300

    Pruebas sobre sumatoria de arreglo  - clase 67

--- tests/helpers/sum.test.ts
+++ tests/helpers/sum.test.ts
@@ -1,13 +1,63 @@
-import { expect, test } from 'vitest';
-import { sum } from '../../src/helpers/sum';
+import { describe, expect, test } from 'vitest';
+import { sum, addArray } from '../../src/helpers/sum';
 
-test('adds 1 + 2 to equal 3', () => {
-  const a = 1;
-  const b = 2;
+describe('add sum of functions', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    // Prepara
+    const a = 1;
+    const b = 2;
 
-  //Estimulo
-  const result = sum(a, b);
+    // Estímulo
+    const result = sum(a, b);
 
-  //Esperado
-  expect(result).toBe(a + b);
+    // Esperado
+    expect(result).toBe(a + b);
+  });
+
+  test('adds -1 + -2 to equal -3', () => {
+    // Prepara
+    const a = -1;
+    const b = -2;
+
+    // Estímulo
+    const result = sum(a, b);
+
+    // Esperado
+    expect(result).toBe(a + b);
+  });
+});
+
+describe('addArray sum of functions', () => {
+  test('adds [1, 2, 3] to equal 6', () => {
+    // Prepara
+    const a = [1, 2, 3];
+
+    // Estímulo
+    const result = addArray(a);
+
+    // Esperado
+    expect(result).toBe(6);
+  });
+
+  test('adds [-1, -2, -3] to equal -6', () => {
+    // Prepara
+    const a = [-1, -2, -3];
+
+    // Estímulo
+    const result = addArray(a);
+
+    // Esperado
+    expect(result).toBe(-6);
+  });
+
+  test('should return 0 for an empty array', () => {
+    // Prepara
+    const a: number[] = [];
+
+    // Estímulo
+    const result = addArray(a);
+
+    // Esperado
+    expect(result).toBe(0);
+  });
 });
